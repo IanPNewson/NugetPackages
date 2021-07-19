6a3550aaed8332fc624202ba5ca6f0c3d01f4995
Ian <ian.newson@gmail.com>
2021-07-19 21:12:47 +0100

    Added a method to get a rectangles center

---
+++ INHelpers/ExtensionMethods/DrawingExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace INHelpers.ExtensionMethods
+{
+
+    /// <summary>
+    /// Extensiom methods for classes or related in the System.Drawing namespace
+    /// </summary>
+    public static class DrawingExtensionMethods
+    {
+
+        public static Point GetCenter(this Rectangle rect)
+        {
+            return new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+        }
+
+    }
+}
